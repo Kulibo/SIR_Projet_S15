1f480440ea38bc1ca22c330a9c28427f2dfdd12c
Kulibo <thomas.prenleloup@laposte.net>
2018-10-30 10:54:25 +0100

    Implementation of the Mcqueen class

--- src/mcqueen/src/library.hpp
+++ src/mcqueen/src/library.hpp
@@ -1,6 +1,8 @@
 #ifndef MCQUEEN_LIBRARY_HPP
 #define MCQUEEN_LIBRARY_HPP
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 namespace mq {
     template <typename T>
@@ -8,17 +10,43 @@ namespace mq {
             public:
                 using vectors = std::vector<T>;
             public:
-                Mcqueen(size_t nPrototypes);
+                Mcqueen(size_t nPrototypes, std::function<double(T)> distance, double step = 0.5);
                 vectors update(T sample);
 
             private:
-                size_t find(T sample) const;
+                typename std::vector<T>::iterator find(T sample) const;
 
             private:
                 vectors _prototypes;
+                std::function<double(T)> _distance;
+                double _step;
         };
 }
 
 
+namespace mq{
+    template<typename T>
+    Mcqueen<T>::Mcqueen(size_t nPrototypes, std::function<double(T)> distance, double step )
+    : _prototypes(nPrototypes)
+    , _distance(distance)
+    , _step(step)
+    {
+        
+    }
+    template<typename T>
+    typename std::vector<T>::iterator Mcqueen<T>::find(T sample) const{
+        return std::min_element(_prototypes.begin(), _prototypes.end(), [this, sample](T a, T b){return _distance(a-sample)<=_distance(b-sample);}); 
+    }
+
+    template<typename T>
+    std::vector<T> Mcqueen<T>::update(T sample){
+        auto prototype = find(sample);
+        *prototype += _step*(sample - *prototype);
+        return _prototypes;
+    }
+
+    
+
+}
 
 #endif
