7d1fe3c3357a02dd1bb8ee3ced85a7ff0bccf09c
Kulibo <thomas.prenleloup@laposte.net>
2018-10-30 10:01:03 +0100

    Created class Mcqueen

--- src/mcqueen/src/library.hpp
+++ src/mcqueen/src/library.hpp
@@ -1,6 +1,24 @@
 #ifndef MCQUEEN_LIBRARY_HPP
 #define MCQUEEN_LIBRARY_HPP
+#include <vector>
 
-void hello();
+namespace mq {
+    template <typename T>
+        class Mcqueen{
+            public:
+                using vectors = std::vector<T>;
+            public:
+                Mcqueen(size_t nPrototypes);
+                vectors update(T sample);
+
+            private:
+                size_t find(T sample) const;
+
+            private:
+                vectors _prototypes;
+        };
+}
+
+
 
 #endif
