b436de0957c6872c705dbc3e1351a9281a9cc2cc
Benjamin Mouscadet <benjamin.mouscadet@supelec.fr>
2018-11-28 09:40:37 +0100

    Ajout de commentaires.

--- src/mcqueen_cv/src/mcqueen_cv.cpp
+++ src/mcqueen_cv/src/mcqueen_cv.cpp
@@ -28,11 +28,27 @@ namespace mq {
 
     void Mcqueen_CV::select_prototypes(size_t proto_index)
     {
+	// Vérifie que l'on demande un index correct
 	if(proto_index >= _learner.prototypes().size())
 		throw std::logic_error("Not enough prototypes");
 
+	// Met le prototype à jour
 	_sel_prot = proto_index;
+
+	// On a modifié le prototype considéré, met à jour le masque
+	update_binary_mask();
     }
+
+    cv::Mat Mcqueen_CV::binay_mask() const
+    {
+        return _binary_mask;
+    }
+
+    void Mcqueen_CV::update_binary_mask()
+    {
+	// Met à jour le masque
+    }
+
 
 }
 	
--- src/mcqueen_cv/src/mcqueen_cv.hpp
+++ src/mcqueen_cv/src/mcqueen_cv.hpp
@@ -21,17 +21,29 @@ namespace mq{
 
     class Mcqueen_CV{
         public:
+	    // Constructeurs
             Mcqueen_CV(cv::Mat img, std::vector<rgb> prototypes);
             Mcqueen_CV(std::string img_filename, std::vector<rgb> prototypes);
             Mcqueen_CV(cv::Mat img, size_t nPrototypes = 10);
             Mcqueen_CV(std::string img_filename, size_t nPrototypes = 10);
+	    // Calcul des clusters dans _learner
             std::vector<rgb> update();
+	    // Réinitialisation aléatoire des prototypes
             std::vector<rgb> reinit_prototypes();
         public:
+	    // Choisit un prototype par son index
 	    void select_prototype(size_t proto_index);
+	    // Renvoie le masque binaire correspondant au prototype considéré
+	    cv::Mat binary_mask() const;
+
+	private:
+	    // Calcule le masque binaire correspondant au prototype considéré
+	    void update_binary_mask();
+
         private:
             std::unique_ptr<Color_selector> _selector;
             Mcqueen _learner;
             size_t _sel_prot;
+	    cv::Mat _binary_mask;
     };
 }
